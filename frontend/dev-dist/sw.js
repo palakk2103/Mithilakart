935eddb68c9e1ea3532b74b3059578121689c820
Palak Patel <palakk2103@gmail.com>
2026-07-28 10:50:57 +0530

    backend file

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-3b23c8c3'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.is6tqqkv2r4"
+    "revision": "0.a5kvqa9sjl8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
